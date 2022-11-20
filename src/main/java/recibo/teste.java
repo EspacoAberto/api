8101c15ba6f5eb48debf6f46d0ab3a0f15447d14
Vinifd1234 <vinicius.pedroso@bandtec.com.br>
2022-11-20 14:40:56 -0300

    Adicionado campo de acomodações na exportação txt e CSV

--- src/main/java/recibo/teste.java
+++ src/main/java/recibo/teste.java
@@ -12,14 +12,16 @@ public class teste {
 
         List<Recibo> lista = new ArrayList();
         ListaObj<Recibo> l = new ListaObj<>(10);
-        lista.add(new Recibo(1,"a","a","a",1,1,1,1.0));
-        lista.add(new Recibo(2,"b","b","b",2,2,2,2.0));
+        lista.add(new Recibo(1,"a","a","a",1,1,1,1.0, 777));
+        lista.add(new Recibo(2,"b","b","b",2,2,2,2.0, 777));
+        lista.add(new Recibo(3,"b","b","b",2,2,2,2.0, 777));
 
-        l.adiciona(new Recibo(1,"a","a","a",1,1,1,1.0));
-//            a.gravaReciboTxt(lista, "alunes.txt");
-//            a.leReciboTxt("alunes.txt");
+        l.adiciona(new Recibo(1,"a","a","a",1,1,1,1.0, 777));
+        l.adiciona(new Recibo(2,"raffa moreira","a","a",1,1,1,1.0, 777));
+       //    a.gravaReciboTxt(lista, "alunes.txt");
+          // a.leReciboTxt("alunes.txt");
 
-//           a.gravaReciboCsv(l,"recibo");
+        // a.gravaReciboCsv(l,"recibo");
             a.leExibeReciboCsv("recibo");
     }
 }
