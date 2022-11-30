04b85a23b595fa2b5c84883742ae9366b81296d2
Nickolas Junior <nickolas.goncalves@bandtec.com.br>
2022-11-30 14:51:46 -0300

    adcionado getter e setter do lombok na classe user

--- src/main/java/espacoaberto/backend/abstrato/Usuario.java
+++ src/main/java/espacoaberto/backend/abstrato/Usuario.java
@@ -2,6 +2,7 @@ package espacoaberto.backend.abstrato;
 
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
@@ -10,6 +11,8 @@ import javax.validation.constraints.Past;
 import java.time.LocalDate;
 
 @Entity
+@Getter
+@Setter
 @Inheritance(strategy=InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name="TYPE")
 public abstract class Usuario {
@@ -26,88 +29,9 @@ public abstract class Usuario {
     @Email
     private String email;
     private String senha;
+
+    private String codigo;
     private Boolean isAutenticado;
 
-    // private Boolean isAnunciante;
-
-    //constructor
-
-    //methods abstract
     public abstract void tornarPremium();
-
-    //get and sett
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public LocalDate getDataNascimento() {
-        return dataNascimento;
-    }
-
-    public void setDataNascimento(LocalDate dataNascimento) {
-        this.dataNascimento = dataNascimento;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public Boolean getPremium() {
-        return isPremium;
-    }
-
-    public void setPremium(Boolean premium) {
-        isPremium = premium;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
-
-    public Boolean getAutenticado() {return isAutenticado;}
-
-    public void setAutenticado(Boolean autenticado) {isAutenticado = autenticado;}
-
-    public abstract Boolean autenticar(String email, String senha);
-
-    @Override
-    public String toString() {
-        return "Usuario{" +
-                "id=" + id +
-                ", nome='" + nome + '\'' +
-                ", dataNascimento=" + dataNascimento +
-                ", cpf='" + cpf + '\'' +
-                ", isPremium=" + isPremium +
-                ", email='" + email + '\'' +
-                ", isAutenticado=" + isAutenticado +
-                ", senha='" + senha + '\'' +
-                '}';
-    }
 }
