73138f36ca6f568400bc75e4c4b4aeeb9b8934a3
juansantiago58 <juan.diaz@bandtec.com.br>
2022-12-07 17:30:10 -0300

    ajustesss

--- src/main/java/espacoaberto/backend/entidades/Imagem.java
+++ src/main/java/espacoaberto/backend/entidades/Imagem.java
@@ -1,2 +1,24 @@
-package espacoaberto.backend.entidades;public interface Imagem {
+package espacoaberto.backend.entidades;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+public class Imagem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idImagem;
+
+    @ManyToOne(cascade=CascadeType.PERSIST)
+    @JoinColumn(name="anuncio_id")
+    private Anuncio anuncio;
+
+    @JsonIgnore
+    @Column(length = 50 * 1024 * 1024) // 50 Mega Bytes
+    private byte[] foto;
 }
