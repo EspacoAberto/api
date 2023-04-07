c7e79bad764b364a51d493eedbdb49d12ae5b80e
nickjng <nickolas.goncalves@bandtec.com>
2023-04-06 22:03:05 -0300

    agendamento_corrigido

--- src/main/java/espacoaberto/backend/entidades/Cliente.java
+++ src/main/java/espacoaberto/backend/entidades/Cliente.java
@@ -1,14 +1,25 @@
 package espacoaberto.backend.entidades;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import espacoaberto.backend.abstrato.Usuario;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Cliente extends Usuario {
 //    private List<Imovel> favoritos;
 
+
+    @ManyToOne
+    @JsonProperty
+    private Agendamento agendamento;
+
     //constructor
+
 
     //methods
     @Override
