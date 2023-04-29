4f5d9e86f7b60e6b6a943ca2c9437be5b274a169
rogerio-alvesf <rogerio.filho@bandtec.com.br>
2023-04-29 16:39:42 -0300

    Adicionando método que deleta o anuncio e ajustando erro na carteira.

--- src/main/java/espacoaberto/backend/repository/AgendamentoRepository.java
+++ src/main/java/espacoaberto/backend/repository/AgendamentoRepository.java
@@ -2,6 +2,9 @@ package espacoaberto.backend.repository;
 
 import espacoaberto.backend.entidades.Agendamento;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface AgendamentoRepository extends JpaRepository<Agendamento, Integer> {
 }
